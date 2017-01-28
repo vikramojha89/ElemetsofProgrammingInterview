c988f9103b493979626ed3e002a84dfbc423eb2c
vikramojha89 <sc.vikram.ojha@gmail.com>
2017-01-28 17:45:49 +0530

    Program to add two numbers without using + operator

--- BinaryOperation/AddTwoNumbers.cpp
+++ BinaryOperation/AddTwoNumbers.cpp
@@ -4,6 +4,32 @@
 #include<iostream>
 using namespace std;
 
+//Function Prtotype
+int Add(int x, int y);
+
+//Driver Code
 int main()
 {
-  
+ int x,y;
+ cout<<"\nEnter two numbers to be added:";
+ cin>>x>>y;
+ int res = Add(x, y);
+ cout <<"\n Sum of "<<x <<"and "<<y<<" is:"<<res<<"\n"; 
+}
+
+//Main Function to add two numbers
+int Add(int x, int y)
+{
+  int sum = 0;
+  int carry=0;
+
+  for(int i=0; i<=64; i++)
+  {
+    sum |= ((x&1) ^ (y&1) ^ carry)<<i;
+    carry = ((x&1)&(y&1)|  //Checking if both x and y has LSB 1
+              (x&carry) | 
+              (y&carry));
+    x>>=1; y>>=1;
+  }
+  return sum;
+}
